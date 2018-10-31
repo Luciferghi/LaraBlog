ed39732185ff2a244b25d54a81a93311da437969
unknown <nitish.ghimire1@gmail.com>
2018-10-31 23:21:21 +0545

    finished authentication except password resetting

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 //Authentication routes
 
-Route::get('auth/login','Auth\LoginController@showLoginForm');
+Route::get('auth/login',['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
 Route::post('auth/login','Auth\LoginController@Login');
 Route::get('auth/logout', 'Auth\LoginController@logout')->name('logout');
 
